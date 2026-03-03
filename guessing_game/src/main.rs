8b95cc937cc49e73070a87ffbf6aa0f7f8f9dfe2
YinYang117 <slcryan2010@gmail.com>
2026-03-03 16:33:29 -0700

    learning std method like cmp, and branches of an enum

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,7 +1,14 @@
 use std::io;
 
+// use std::cmp::Ordering;
+
+use rand::Rng;
 fn main() {
     println!("Guess the Number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=10);
+
+    println!("The secret number is {secret_number}");
 
     println!("Input your guess");
 
@@ -11,5 +18,14 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
     println!("You guessed: {guess}");
+
+    match guess.cmp(&secret_number) {
+        std::cmp::Ordering::Less => println!("Too small!"),
+        std::cmp::Ordering::Greater => println!("Too big!"),
+        std::cmp::Ordering::Equal => println!("You win!")
+        // or uncomment the use on line 3, and cut std::cmp::
+    }
 }
