eade1e1b7fedb02a1fa4eddd311f11713cb5ab6d
YinYang117 <slcryan2010@gmail.com>
2026-03-04 13:44:32 -0700

    loop

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -10,22 +10,28 @@ fn main() {
 
     println!("The secret number is {secret_number}");
 
-    println!("Input your guess");
+    loop {
 
-    let mut guess = String::new();
-
-    io::stdin()
+        println!("Input your guess");
+        
+        let mut guess = String::new();
+        
+        io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
-
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
-
-    println!("You guessed: {guess}");
-
-    match guess.cmp(&secret_number) {
-        std::cmp::Ordering::Less => println!("Too small!"),
-        std::cmp::Ordering::Greater => println!("Too big!"),
-        std::cmp::Ordering::Equal => println!("You win!")
-        // or uncomment the use on line 3, and cut std::cmp::
+    
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        
+        println!("You guessed: {guess}");
+        
+        match guess.cmp(&secret_number) {
+            std::cmp::Ordering::Less => println!("Too small!"),
+            std::cmp::Ordering::Greater => println!("Too big!"),
+            std::cmp::Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+            // or uncomment the use on line 3, and cut std::cmp::
+        }
     }
 }
