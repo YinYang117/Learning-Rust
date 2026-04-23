f44679d70bed80e917177b77a16c7d23463453fc
YinYang117 <slcryan2010@gmail.com>
2026-04-23 00:42:27 -0600

    arrays and tuples finished

--- data_types_plus/src/main.rs
+++ data_types_plus/src/main.rs
@@ -5,15 +5,34 @@ fn main() {
 
     // let test: i32 = num1 * num2; // can't * an i32 and a f32
 
-    let modulo_test: i32 = 10;
-    let res = modulo_test % 211;
-    println!("res: {}", res);
+    // let modulo_test: i32 = 10;
+    // let res = modulo_test % 211;
+    // println!("res: {}", res);
 
-    let tup: (i32, f32) = (-45, 3.8);
-    println!("tup, {:?}", tup);
+    // let tup: (i32, f32) = (-45, 3.8);
+    // println!("tup, {:?}", tup);
 
-    let tup2: (char, i32) = ('😻', 5);
-    println!("{:?}", tup2);
+    // let mut tup2: (char, i32) = ('😻', 5);
+    // println!("tup2 {:?}", tup2);
+    // {
+    //     tup2.1 += 6;
+    //     println!("in cur {}", tup2.1);
+    // }
+    // println!("tup2 {:?}", tup2);
+
+    let mut arr = [1, 2, 3, 4, 5];
+    let mut zero_idx = arr[0];
+    println!("mut pre {}", zero_idx);
+    zero_idx += 11;
+    arr[4] += 289;
+    println!("mut aft {}", zero_idx);
+    println!("arr {:?}", arr);
+
+    
+  let t = ([1; 2], [3; 4]); // ([1, 1], [3, 3, 3, 3])
+  let (a, b) = t; // destructuring
+  println!("{}", a[0] + t.1[0]); // will print 4.
+
 }
 
 // What is the difference between scalar and compound
