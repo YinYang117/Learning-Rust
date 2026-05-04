b9f24aaa58a6eb1628790e052b16dbdd5a1a6f32
YinYang117 <slcryan2010@gmail.com>
2026-05-04 13:25:24 -0600

    len macro

--- loops/main.rs
+++ loops/main.rs
@@ -1,10 +1,17 @@
 fn main () {
-    let mut counter = 1;
-    while counter == 1 {
-        counter += 1;
-        println!("{}", counter);
-        if counter == 10 {break};
-        println!("after return")
-    };
-    println!("done");
+    // let mut counter = 1;
+    // while counter == 1 {
+    //     counter += 1;
+    //     println!("{}", counter);
+    //     if counter == 10 {break};
+    //     println!("after return")
+    // };
+    // println!("done");
+
+    let arr = [1, 2, 3, 4, 5];
+    let len = arr.len();
+    println!("{len}");
+    for idx in arr {
+        println!("val = {idx}")
+    }
 }
